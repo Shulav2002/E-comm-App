72cbd4ab069e857e28320dc8937fb59e6e22ebf0
Shulav Dangol <shulav.dangol@gmail.com>
2024-12-19 20:29:23 +0545

    gradle fixes

--- lib/Widgets/banner-widget.dart
+++ lib/Widgets/banner-widget.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_controller.dart' as slider;
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_slider/carousel_options.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -12,32 +14,40 @@ class BannerWidget extends StatefulWidget {
   const BannerWidget({super.key});
 
   @override
-  State<BannerWidget> createState() => _BannerWidgetState();
+  _BannerWidgetState createState() => _BannerWidgetState();
 }
 
 class _BannerWidgetState extends State<BannerWidget> {
-  final CarouselController carouselController = CarouselController();
+  final slider.CarouselController _controller = slider.CarouselController();
   final bannerController _bannerController = Get.put(bannerController());
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Obx(() {
+        if (_bannerController.bannerUrls.isEmpty) {
+          return Center(
+            child: Text("No banners available", style: TextStyle(fontSize: 16)),
+          );
+        }
+
         return CarouselSlider(
           items: _bannerController.bannerUrls
               .map(
-                (imageUrls) => ClipRRect(
+                (imageUrl) => ClipRRect(
                   borderRadius: BorderRadius.zero,
                   child: CachedNetworkImage(
-                    imageUrl: imageUrls,
+                    imageUrl: imageUrl,
                     fit: BoxFit.cover,
-                    width: Get.width - 1,
+                    width: Get.width,
                     placeholder: (context, url) => ColoredBox(
                       color: Colors.black,
                       child: Center(
                         child: CupertinoActivityIndicator(),
                       ),
                     ),
-                    errorWidget: (context, url, error) => Icon(Icons.error),
+                    errorWidget: (context, url, error) =>
+                        Icon(Icons.error, color: Colors.red),
                   ),
                 ),
               )
@@ -45,10 +55,10 @@ class _BannerWidgetState extends State<BannerWidget> {
           options: CarouselOptions(
             scrollDirection: Axis.horizontal,
             autoPlay: true,
-            aspectRatio:
-                16 / 9, // Adjust this to match the aspect ratio of your images
+            aspectRatio: 16 / 9, // Adjust to match the image aspect ratio
             viewportFraction: 1,
           ),
+          carouselController: _controller,
         );
       }),
     );
